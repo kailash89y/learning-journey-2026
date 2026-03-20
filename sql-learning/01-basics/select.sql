7f2cf8e34737ed917511955c6b76c1c18379ab52
Kailash Reddy <kailashreddy2008@gmail.com>
2026-03-20 17:45:49 +0530

    Day 1 – Added SELECT queries content

--- sql-learning/01-basics/select.sql
+++ sql-learning/01-basics/select.sql
@@ -1,1 +1,10 @@
+-- Day 1: SELECT Basics
 
+-- Select all columns
+SELECT * FROM customers;
+
+-- Select specific columns
+SELECT name, email FROM customers;
+
+-- Notes:
+-- SELECT is used to retrieve data from a table
